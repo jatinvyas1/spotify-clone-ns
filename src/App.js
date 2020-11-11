faee889f200a943a7735cc1dae8db052da1d8c60
jatinvyas1 <jatin.vyas@outlook.com>
2020-11-11 21:36:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Login from "./Login";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>This is my spotify.</h1>
+      <Login/>
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Login.css"
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt="logo"/>
+            <a>Login with Spotify</a>
+        </div>
+    )
+}
+
+export default Login
