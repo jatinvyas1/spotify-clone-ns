1d76ace0c7d2208cb1133a760c31f478145289a4
jatinvyas1 <jatin.vyas@outlook.com>
2020-11-12 15:32:11 +0530

    dataLayer setUP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useEffect,useState}from 'react'
 import {loginURL,getToken} from './spotify';
 import Login from "./Login";
 import SpotifyWebAPI from "spotify-web-api-js";
+import {useStateProviderValue} from "./StateProvider";
 import Player from "./Player"
 import "./styles.css"
 const spotify = new SpotifyWebAPI();
@@ -9,6 +10,8 @@ const spotify = new SpotifyWebAPI();
 
 export default function App() {
   const [token,setToken] = useState(null);
+  const [{user},dispath] = useStateProviderValue();
+
     useEffect(() => {
         const hash = getToken();
         window.location.hash = "";
@@ -19,6 +22,10 @@ export default function App() {
             //To get User Acc
             spotify.getMe().then(user=>{
               console.log(user);
+              dispatchEvent({
+                type:'SET_USER',
+                user:user
+              })
             })
         }
        }, [])
